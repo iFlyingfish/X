c6da5d42988a2be240d109763bac491f81a6a5fe
LeoFerrari <leoferrari19@gmail.com>
2015-02-16 17:31:30 +0800

    Build The StateMachine,Reorganize The Architecture

---
+++ frameworks/runtime-src/proj.ios_mac/XCharacter.cpp
@@ -0,0 +1,56 @@
+//
+//  XCharacter.cpp
+//  X
+//
+//  Created by leo on 15/2/16.
+//
+//
+
+#include "XCharacter.h"
+#include "XCharacterSprite.h"
+#include "XStateMachine.h"
+
+XCharacter::XCharacter(XCharcterSprite* characterSprite,int HP)
+:mCharcterSprite(characterSprite)
+,mHP(HP)
+{
+   
+}
+
+XCharacter::~XCharacter()
+{
+    XStateMachineListItr it = mStateMachineList.begin();
+    while (it != mStateMachineList.end()) {
+        ++it;
+    }
+}
+
+
+bool XCharacter::init()
+{
+    return true;
+}
+
+void XCharacter::tick()
+{
+    XStateMachineListItr it = mStateMachineList.begin();
+    while (it != mStateMachineList.end()) {
+       // (*it)->tick();
+        ++it;
+    }
+}
+
+XCharcterSprite* XCharacter::getCharacterSprtie()
+{
+    return mCharcterSprite;
+}
+
+void XCharacter::setHP(int HP)
+{
+    mHP = HP;
+}
+
+int XCharacter::getHP()
+{
+    return mHP;
+}
